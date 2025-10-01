d2bb1a71a0cdd8298ab7303ff8796173971beaac
mathiasur <murtheil@outlook.com>
2025-10-01 17:04:38 +0000

    agregar regex para links e imagenes

--- src/main.py
+++ src/main.py
@@ -3,19 +3,14 @@ from htmlnode import *
 from leafnode import *
 from parentnode import *
 from utils import *
+import re
 
 
 def main():
-    
-    node = TextNode("`This is text` `with` a code block word", TextType.TEXT)
-    node2 = TextNode("`This is another text` with a code block word", TextType.TEXT)
-    
-    positions = split_nodes_delimiter([node, node2], "`", TextType.CODE)
-    
-    for position in positions:
-        print(position)
-    
 
+    text = "This is text with a link [to boot dev](https://www.boot.dev) and [to youtube](https://www.youtube.com/@bootdotdev)"
+    print(extract_markdown_links(text))
+# [("to boot dev", "https://www.boot.dev"), ("to youtube", "https://www.youtube.com/@bootdotdev")]
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,4 @@
+import re
 from textnode import *
 
 
@@ -53,3 +54,10 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         new_nodes += split_node(node, delimiter, text_type)
 
     return new_nodes
+
+
+def extract_markdown_images(text):
+    return re.findall(r"!\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
+
+def extract_markdown_links(text):
+    return re.findall(r"(?<!!)\[([^\[\]]*)\]\(([^\(\)]*)\)", text)
